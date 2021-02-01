921c74fd948312cbc931a09b8d19fd42160e4515
Guy Zohar <zguyzohar@gmail.com>
2021-02-01 16:13:10 +0200

    update ui to guy zohar instead

---
+++ js/selector-projects.js
@@ -0,0 +1,46 @@
+var gProjects = [
+    {
+        id: 'yelpcamp',
+        name: 'YelpCamp',
+        title: 'Review camp sites across the world!',
+        desc:
+            'YelpCamp is my first official project I have made.  Specifically made to practice my basic fullstack development skills, Using Nodejs, Express, and Basic api handling I wanted to get a certain feel of the Web Development field. This site contains the full CRUD aspect of a site, and has been used with mongoDB. The app has been made to improve the convenience for camping lovers around the world, a place to find new amazing camp sites, and leave a review about them.',
+        url: [],
+        imgUrl: '',
+        createdAt: '12/11/20',
+    },
+    {
+        id: 'touchnums',
+        name: 'TouchNums',
+        title: 'Hope you get that streak right!',
+        desc:
+            "After I finished YelpCamp, I decided it's time to take a step back and improve my fundamentals. At first focusing with basic javascript I created TouchNums, A game that shows the improve in basic functioning in front end development and the use of 2D arrays.",
+        url: [],
+        imgUrl: '',
+        createdAt: '12/01/21',
+    },
+    {
+        id: 'minesweeper',
+        name: 'Minesweeper',
+        title: 'Watch out for the mines!',
+        desc:
+            'After my first big project I wanted to improve my basic skills and become more proffesional. 2 months after diving deeper into Javascript I came up with this project. What better way to show my Javascript fundamentals than with a game that deals with 2Dimensional Arrays and loops around the whole board?, I cloned by scratch the windows XP Minesweeper game with some added features and styles appropriate for our time and age.',
+        url: [],
+        imgUrl: '',
+        createdAt: '24/01/21',
+    },
+    {
+        id: 'todo',
+        name: 'To-Do List',
+        title: 'Keep yourself organized.',
+        desc:
+            'Now it was time to get better with basic CRUD functioning. This time without back end and with manipulation of the local storage instead. This step has felt for me mandatory to understanding the CRUD aspect even better than before.',
+        url: [],
+        imgUrl: '',
+        createdAt: '27/01/21',
+    }
+];
+
+function getProjects(){
+    return gProjects;
+}
